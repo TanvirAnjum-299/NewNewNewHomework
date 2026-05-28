1f10b154a1a03dd69a2ce244024e6666c6622dd8
TanvirAnjum-299 <sinthiaibnat79@gmail.com>
2026-05-28 18:00:03 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,73 +1,46 @@
-# importing the module
-import sys
+print("\t\tSMARTPHONE DIRECTORY", flush=False)
 
-# this function will be the first to run as soon as the main function executes
-def initial_slambook():
-    rows, cols = int(input("Please enter number of yours: ")), 5
+print("\tYou can now perform the following operation on this\nslambook\n")
+print("1. Add a new contact")
+print("6. Exit phonebook")
 
-    # We are collecting the initial number of contacts the user wants to have in the
-    # phonebook already. User may also enter 0 if he doesn't wish to enter any.
-    slam_book = []
-    print(slam_book)
+def add_contact(pb):
+    # Adding a contact is the easiest because all you need to do
+    # is: append another list of details into the already existing list
+    dip = []
+    for i in range(len(pb[0])):
+        if i == 0:
+            dip.append(str(input("Enter name: ")))
+        if i == 1:
+            dip.append(int(input("Enter number: ")))
+        if i == 2:
+            dip.append(str(input("Enter e-mail address: ")))
+        if i == 3:
+            dip.append(str(input("Enter date of birth(dd/mm/yy): ")))
+        if i == 4:
+            dip.append(str(input("Enter category (Family/Friends/work/Others): ")))
+    pb.append(dip)
+    # And once you modify the list, you return it to the calling function, here
+    return pb
+def thanks():
+    # A simple gesture of courtesy towards the user to enhance user experience
+    print("******************************************************")
+    print("Thank you for using our Slam Book.")
+    print("Please visit again!")
+    print("******************************************************")
+    sys.exit("Goodbye, have a nice day ahead!")
 
-    for i in range(rows):
-        print("\nEnter contact %d details in the following order (ONLY): " % (i+1))
-        print("NOTE: * indicates mandatory fields")
-        print ("………………………………………………………………………………")
-
-        temp = []
-        for j in range(cols):
-            # We have taken the conditions for values of j only for the personalized fields
-            # such as name, number, e-mail id, dob, category etc
-            if j == 0:
-                temp.append(str(input("Enter name*: ")))
-                # We need to check if the user has left the name empty
-                # name & number are mandatory fields.
-                # So implement a condition to check as below.
-                if temp[j] == '' or temp[j] == ' ':
-                    sys.exit(
-                        "Name is a mandatory field. Process exiting due to blank field..."
-                    )
-                # This will exit the process if a blank field is encountered.
-
-            if j == 1:
-                temp.append(int(input("Enter number*: ")))
-                # We do not need to check if user has entered the number because int automatically
-                # takes care of it. Int value cannot accept a blank as that counts as a string.
-                # So process automatically exits without us using the sys package.
-
-            if j == 2:
-                temp.append(str(input("Enter something about your friend: ")))
-                # Even if this field is left as blank, None will take the blank's place
-                if temp[j] == '' or temp[j] == ' ':
-                    temp[j] = None
-
-            if j == 3:
-                temp.append(str(input("Enter date of birth(dd/mm/yy): ")))
-                # Whatever format the user enters dob in, it won't make
-                if temp[j] == '' or temp[j] == ' ':
-                    # Even if this field is left as blank, None will take the blank's place
-                    temp[j] = None
-
-            if j == 4:
-                temp.append(
-                    str(input("Enter category(Family/Friends/Work/Others): "))
-                )
-                # Even if this field is left as blank, None will take the blank's place
-
-                if temp[j] == '' or temp[j] == ' ':
-                    temp[j] = None
-
-        slam_book.append(temp)
-        # By this step we are appending a list temp into a list phone_book
-        # That means phone_book is a 2-D array and temp is a 1-D array
-
-    print(slam_book)
-    return slam_book
-
-def menu():
-    # We created this simple menu function for
-    # code reusability & also for an interactive console
-    # Menu func will only execute when called
-    print("**********************************************************************")
-    
+# Main function code
+print("........................................................")
+print("Hello dear Friends, welcome to our Slam Book")
+print("You may now proceed to explore this Slam Book and fill your details about your friends")
+# This is solely meant for decoration purpose only.
+# You're free to modify your interface as per your will to make it look interactive
+ch = 1
+pb = initial_slambook()
+while ch in (1, 2, 3, 4, 5):
+    ch = menu()
+    if ch == 4:
+        pb = add_contact(pb)
+    else:
+        thanks()
